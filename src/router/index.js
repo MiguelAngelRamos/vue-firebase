dcc0dc75cf5613a7720150893666b10878004c37
Miguel Angel Ramos <return.infox@gmail.com>
2024-05-23 10:21:23 -0400

    formulario de login

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -9,7 +10,15 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/LoginView.vue')
+    }
   ]
 })
 
 export default router
+
+// http://localhost:5173/
+// http://localhost:5173/login
---
+++ src/views/LoginView.vue
@@ -0,0 +1,18 @@
+<script setup>
+  
+</script>
+
+<template>
+  <v-card flat max-width="500" class="mx-auto my-10">
+    <v-card-title class="text-center text-h4 font-weight-bold" tag="h4">Iniciar Sesión</v-card-title>
+    <v-form>
+      <v-text-field type="email" label="Email" class="mb-3"/>
+      <v-text-field type="password" label="Password"/>
+      <v-btn block color="deep-purple-darken-3">Iniciar Sesión</v-btn>
+    </v-form>
+  </v-card>
+</template>
+
+
+<style scoped>
+</style>
