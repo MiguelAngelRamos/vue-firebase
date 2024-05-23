cdb12e5a5d696874b97f11fc0448bc629aca9209
Miguel Angel Ramos <return.infox@gmail.com>
2024-05-23 10:47:25 -0400

    formulario de login actualizado

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,14 +1,28 @@
 <script setup>
-  
+  import { useForm, useField } from 'vee-validate';
+  import { loginSchema as validationSchema } from '../validation/loginSchema';
+
+  const { handleSubmit } = useForm({validationSchema});
+
+  const email = useField('email')
+  const password = useField('password')
+
+
+
+  const submit = handleSubmit((values) => {
+    console.log(values);
+  });
+
+
 </script>
 
 <template>
   <v-card flat max-width="500" class="mx-auto my-10">
     <v-card-title class="text-center text-h4 font-weight-bold" tag="h4">Iniciar Sesión</v-card-title>
     <v-form>
-      <v-text-field type="email" label="Email" class="mb-3"/>
-      <v-text-field type="password" label="Password"/>
-      <v-btn block color="deep-purple-darken-3">Iniciar Sesión</v-btn>
+      <v-text-field type="email" label="Email" class="mb-3" v-model="email.value.value" :error-messages="email.errorMessage.value"/>
+      <v-text-field type="password" label="Password" v-model="password.value.value" :error-messages="password.errorMessage.value"/>
+      <v-btn block color="deep-purple-darken-3" @click="submit">Iniciar Sesión</v-btn>
     </v-form>
   </v-card>
 </template>
